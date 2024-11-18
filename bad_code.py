aacdc2b678b5f88c63d09dead1044bca57a4d3d0
Naser Al Madi <77322034+nalmadi@users.noreply.github.com>
2024-11-18 13:49:11 -0500

    Update bad_code.py

--- bad_code.py
+++ bad_code.py
@@ -1,13 +1,25 @@
-# bad_code.py
+'''
+filename: bad_code.py
 
-import math,sys  # Multiple imports on one line
+'''
 
-def calculate_area(radius): 
-    if radius <= 0: print("Radius must be positive!")  # Inline if statement
+
+import math
+
+def calculate_area(radius):
+    '''
+    calcualtes area from radius
+    '''
+    if radius <= 0: 
+        print("Radius must be positive!")  # Inline if statement
     return math.pi*radius**2
 
 def print_area(radius):
-    area=calculate_area(radius)  # Missing spaces around assignment operator
+    '''
+    prints area
+    '''
+    area = calculate_area(radius)  # Missing spaces around assignment operator
     print(f"The area of the circle with radius {radius} is {area}") # Inconsistent indentation
 
-for i in range(5): print_area(i+1) # Inline for loop, should be split onto separate lines
+for i in range(5): 
+    print_area(i+1) # Inline for loop, should be split onto separate line
