9d9dcab8334497522e2f3e11148d2d19766ac727
Naser Al Madi <77322034+nalmadi@users.noreply.github.com>
2024-11-18 13:50:12 -0500

    Update bad_code.py

--- bad_code.py
+++ bad_code.py
@@ -10,7 +10,7 @@ def calculate_area(radius):
     '''
     calcualtes area from radius
     '''
-    if radius <= 0: 
+    if radius <= 0:
         print("Radius must be positive!")  # Inline if statement
     return math.pi*radius**2
 
@@ -21,5 +21,5 @@ def print_area(radius):
     area = calculate_area(radius)  # Missing spaces around assignment operator
     print(f"The area of the circle with radius {radius} is {area}") # Inconsistent indentation
 
-for i in range(5): 
+for i in range(5):
     print_area(i+1) # Inline for loop, should be split onto separate line
